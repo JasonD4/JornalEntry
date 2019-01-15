e27251aa3a29478b8dc86925d5badd45ec631c0e
Jason <Jason@Pursuits-MacBook-Pro-42.local>
2019-01-15 13:34:32 -0500

    photoHelperDone

--- PhotoJornal/PhotoHelpers/PhotoHelpers.swift
+++ PhotoJornal/PhotoHelpers/PhotoHelpers.swift
@@ -11,10 +11,37 @@ import Foundation
 final class PhotoHelpers{
     
     private static let storePost = "AllUserEntries"
+    private static var picturePost: [PictureModel]?
     private init() {}
     
-    static func saveTheEntry(typeOfFileSaving: PictureModel){
+    static func saveTheEntry(typeOfFileSaving: [PictureModel]){
         
         let pathToSaveTo = DataPersistenceHelper.filepathToDocumentDirectory(filename: storePost)
+        do{
+            let photoData = try PropertyListEncoder().encode(picturePost)
+            try photoData.write(to: pathToSaveTo, options: Data.WritingOptions.atomic)
+        }catch{
+            print("error in the static function saveTheEntry: \(error)")
+        }
     }
+    static func loadTheEntry() -> [PictureModel]?{
+        
+        let path = DataPersistenceHelper.filepathToDocumentDirectory(filename: storePost).path
+        if FileManager.default.fileExists(atPath: path){
+            if let data = FileManager.default.contents(atPath: path){
+                do{// [phototJornal]
+                    picturePost = try PropertyListDecoder().decode([PictureModel].self, from: data)
+                }catch{
+                    print(error)
+                }
+            }else{
+                print("LoadtheEntry func data area is nil")
+            }
+        }
+        else{
+            print("\(storePost) does not exsist")
+        }
+        return picturePost
+    }
+    
 }
